e03d03c5556896d336c9ec4cc817a637f27b9b0b
Guilherme Welter <guilherme.a.welter@gmail.com>
2019-12-03 22:29:28 -0300

    Refactoring create and edit blogpost and using defaultprops

--- src/screens/Create/index.js
+++ src/screens/Create/index.js
@@ -1,53 +1,19 @@
 import React, { useState, useContext } from "react";
-import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
 
+import BlogPostForm from "../../components/BlogPostForm";
 import { Context as BlogContext } from "../../context";
-
-// import { Container } from './styles';
 
 export default function Create({ navigation }) {
   const { addBlogPost } = useContext(BlogContext);
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
 
   return (
-    <View>
-      <Text style={styles.label}>Enter Title</Text>
-      <TextInput
-        style={styles.input}
-        value={title}
-        onChangeText={text => setTitle(text)}
-      />
-      <Text style={styles.label}>Enter Content</Text>
-      <TextInput
-        style={styles.input}
-        value={content}
-        onChangeText={text => setContent(text)}
-      />
-      <Button
-        disabled={!title || !content}
-        title="Add blog post"
-        onPress={() => {
-          addBlogPost(title, content, () => {
-            navigation.goBack();
-          });
-        }}
-      />
-    </View>
+    <BlogPostForm
+      onSubmit={(title, content) =>
+        addBlogPost(title, content, () => navigation.navigate("Index"))
+      }
+    />
   );
 }
 
-const styles = StyleSheet.create({
-  input: {
-    fontSize: 18,
-    borderWidth: 1,
-    borderColor: "black",
-    margin: 5,
-    marginBottom: 15,
-    padding: 5
-  },
-  label: {
-    fontSize: 20,
-    margin: 5
-  }
-});
+const styles = StyleSheet.create({});
--- src/screens/Edit/index.js
+++ src/screens/Edit/index.js
@@ -1,58 +1,22 @@
-import React, { useContext, useState } from "react";
-import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+import React, { useContext } from "react";
+import { StyleSheet } from "react-native";
 
+import BlogPostForm from "../../components/BlogPostForm";
 import { Context as BlogContext } from "../../context";
-
-// import { Container } from './styles';
 
 export default function Edit({ navigation }) {
   const { state, editBlogPost } = useContext(BlogContext);
   const id = navigation.getParam("id");
   const blogPost = state.find(post => post.id === id);
 
-  const [title, setTitle] = useState(blogPost.title);
-  const [content, setContent] = useState(blogPost.content);
-
-  console.log({ id, title, content });
-
   return (
-    <View>
-      <Text style={styles.label}>Enter Title</Text>
-      <TextInput
-        style={styles.input}
-        value={title}
-        onChangeText={text => setTitle(text)}
-      />
-      <Text style={styles.label}>Enter Content</Text>
-      <TextInput
-        style={styles.input}
-        value={content}
-        onChangeText={text => setContent(text)}
-      />
-      <Button
-        disabled={!title || !content}
-        title="Save"
-        onPress={() => {
-          editBlogPost(id, title, content, () => {
-            navigation.goBack();
-          });
-        }}
-      />
-    </View>
+    <BlogPostForm
+      initialValues={{ title: blogPost.title, content: blogPost.content }}
+      onSubmit={(title, content) =>
+        editBlogPost(id, title, content, () => navigation.navigate("Index"))
+      }
+    />
   );
 }
 
-const styles = StyleSheet.create({
-  input: {
-    fontSize: 18,
-    borderWidth: 1,
-    borderColor: "black",
-    margin: 5,
-    marginBottom: 15,
-    padding: 5
-  },
-  label: {
-    fontSize: 20,
-    margin: 5
-  }
-});
+const styles = StyleSheet.create({});
